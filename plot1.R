fc57875d33ddafb9014c62ee5773924189547ee5
pmcgee69 <paulmcgee1969@gmail.com>
2015-06-02 17:10:15 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -30,6 +30,6 @@ hist(hpc$Global_active_power, xlab="Global Active Power (kilowatts)",
                               ylab="Frequency", 
                               main="Global Active Power", 
                               col="red")
+## Done
 dev.off()
 
-## Done
